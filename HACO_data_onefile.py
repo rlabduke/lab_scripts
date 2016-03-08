1a71287e5a58516b1e5f7f3163e0d0e88502b3ba
Steven Lewis <smlewi@gmail.com>
2016-03-08 12:57:59 -0500

    add os.system to run commands.  capture output into a result file

--- HACO_data_onefile.py
+++ HACO_data_onefile.py
@@ -23,25 +23,30 @@ if __name__ == '__main__':
     #generate local copy of file
     gunzip_command = "gunzip --to-stdout " + in_path + " > " + unzipped_path
     print gunzip_command
+    os.system(gunzip_command)
 
     #run Reduce to strip old H
     reduce_stripped_path = cwd + pdb + ".stripped.pdb"
     reduce_strip_command = "phenix.reduce -quiet -trim -allalt " + unzipped_path + " | grep -v '^USER  MOD' > " + reduce_stripped_path
     print reduce_strip_command
+    os.system(reduce_strip_command)
 
     #run Reduce
     reduced_path = cwd + pdb + "H.pdb"
     reduce_command = "phenix.reduce -quiet -nobuild9999 " + reduce_stripped_path + " > " + reduced_path
     print reduce_command
+    os.system(reduce_command)
 
     #remove stripped pdb
     rm_strip_command = "rm " + reduce_stripped_path
     print rm_strip_command
+    os.system(rm_strip_command)
 
     #run CaBLAM
     CaBLAM_results_path = cwd + pdb + "_3CA_angle.txt"
     CaBLAM_command = "phenix.cablam_training cablam=True " + reduced_path + " > " +  CaBLAM_results_path
     print CaBLAM_command
+    os.system(CaBLAM_command)
 
     #get DSSP
     #this is dumb to use FTP, but when I tried rsyncing the whole database I realized they have literally every PDB's dssp in one directory, which causes performance issues
@@ -49,6 +54,7 @@ if __name__ == '__main__':
     dssp_path = cwd + pdb + ".dssp"
     dssp_command = "curl ftp://ftp.cmbi.ru.nl/pub/molbio/data/dssp/" + pdb + ".dssp > " + dssp_path
     print dssp_command
+    os.system(dssp_command)
 
     #cryptic remark
     # Run DSSP on xxxx.pdb
@@ -59,8 +65,11 @@ if __name__ == '__main__':
     probe_path = cwd + pdb + "H_onedot_HACO.txt"
     probe_command = 'phenix.probe -once -mc -Radius1.0 -u -onedot "atom_HA_,atom_HA2 protein" "atom_O__ protein" ' + reduced_path + " > " + probe_path
     print probe_command
+    os.system(probe_command)
 
     # Run Angle_add_HACO.py to combine pertinent data from all three previous runs into one text file and then transfer to an excel sheet for graphing, sorting.
     #Total commandline is "python Angle_add_HACO_02292016.py 3XXXH_cablam3CA_angle.txt 3XXX__dssp_1lineHeader.txt 3XXXH_onedot_1.0rad_All_02292016.txt"
-    HACO_Liz_command = "python Angle_add_HACO.py " + CaBLAM_results_path + " " + dssp_path + " " + probe_path
+    HACO_path = cwd + pdb + ".HACOresult.csv"
+    HACO_Liz_command = "python Angle_add_HACO.py " + CaBLAM_results_path + " " + dssp_path + " " + probe_path + " > " + HACO_path
     print HACO_Liz_command
+    os.system(HACO_Liz_command)
