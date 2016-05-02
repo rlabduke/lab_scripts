e866554e6102481da8abcdda52b5f50ea020ae6e
Steven Lewis <smlewi@gmail.com>
2016-05-02 12:16:28 -0400

    update grepper to do some sed processing too.  update gitignore.

--- grepper.bash
+++ grepper.bash
@@ -5,3 +5,6 @@
 pdbpath=/home/smlewis/whole_PDB_as_PDB_20160429_gunzip
 
 find $pdbpath -name "*ent" -exec grep -H ^SSBOND {} \; > all_ssbond_recs.txt
+
+cat all_ssbond_recs.txt | sed 's>'"$pdbpath"'/[0-9a-z][0-9a-z]/pdb>>g' | sed 's>.ent:>.pdb >g' > all_ssbond_recs.proc.txt
+
