9eda2258d210197aad38a0791a34dbf62c751ba9
Steven Lewis <smlewi@gmail.com>
2016-05-03 13:54:56 -0400

    remove nonCYS ssbond

--- grepper.bash
+++ grepper.bash
@@ -6,5 +6,5 @@ pdbpath=/home/smlewis/whole_PDB_as_PDB_20160429_gunzip
 
 find $pdbpath -name "*ent" -exec grep -H ^SSBOND {} \; > all_ssbond_recs.txt
 
-cat all_ssbond_recs.txt | sed 's>'"$pdbpath"'/[0-9a-z][0-9a-z]/pdb>>g' | sed 's>.ent:>.pdb >g' > all_ssbond_recs.proc.txt
+cat all_ssbond_recs.txt | sed 's>'"$pdbpath"'/[0-9a-z][0-9a-z]/pdb>>g' | sed 's>.ent:>.pdb >g' | grep CYS | all_ssbond_recs.proc.txt
 
