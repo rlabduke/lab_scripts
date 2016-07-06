f83db0ca88a875fb81ec69568a8f9c2addbf744b
Steven Lewis <smlewi@gmail.com>
2016-07-05 22:08:22 -0400

    saving the bit I did on command line

--- cis_in_cazy/cis_in_casy.bash
+++ cis_in_cazy/cis_in_casy.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#this bit gets the cazy search html
 extension=".pdb"
 curl_pre="http://www.cazy.org/search?page=recherche&lang=en&recherche="
 curl_post="&tag=8"
@@ -11,6 +12,13 @@ do
 
 done
 
+#some useful grep commands for parsing the data into yes-and-no bins:
+
+#grep -L "hits" raw_html/* | sed "s:raw_html/::" | sed "s:.html::" > no_hits.csv
+#grep -l "hits" raw_html/* | sed "s:raw_html/::" | sed "s:.html::" > yes_hits.csv
+
+
+#pseudocode for the rest of the project (do this in python)
 # grep hits 1ubq.html
 # if "hits" appears:
 
