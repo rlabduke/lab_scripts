e6a2a7afae50817ed729272f6a62861208adf61f
Steven Lewis <smlewi@gmail.com>
2016-07-05 21:53:45 -0400

    test loop

--- cis_in_cazy/cis_in_casy.bash
+++ cis_in_cazy/cis_in_casy.bash
@@ -1,33 +1,39 @@
-for all pdbs
-curl cazy
-curl "http://www.cazy.org/search?page=recherche&lang=en&recherche=1itx&tag=8" > 1itx.html
+#!/bin/bash
+
+extension=".pdb"
+curl_pre="\"http://www.cazy.org/search?page=recherche&lang=en&recherche="
+curl_post="&tag=8\""
+cat these_pdbs.csv | while read line
+do
+    pdb=$line$extension
+    echo $curl_pre$line$curl_post
+done
+
+# grep hits 1ubq.html
+# if "hits" appears:
+
+# parse these bits out to csv
+
+# <table class="search">
+#                 <tr>
+#                         <th class="thsearch">Family</th>
+#                         <th class="thsearch">Kingdom</th>
+#                         <th class="thsearch">Organism</th>
+#                         <th class="thsearch">Protein Name</th>
+#                 </tr>
+
+#                 <tr>
+#                         <td><a href="http://www.cazy.org/GH18.html">
+#                                         GH18</a></td>
+#                         <td>Bacteria</td>
+#                         <td>Bacillus circulans WL-12</td>
+#                         <td>chitinase A1 (ChiA;ChiA1)</td>
+#                 </tr>
 
 
-grep hits 1ubq.html
-if "hits" appears:
-
-parse these bits out to csv
-
-<table class="search">
-                <tr>
-                        <th class="thsearch">Family</th>
-                        <th class="thsearch">Kingdom</th>
-                        <th class="thsearch">Organism</th>
-                        <th class="thsearch">Protein Name</th>
-                </tr>
-
-                <tr>
-                        <td><a href="http://www.cazy.org/GH18.html">
-                                        GH18</a></td>
-                        <td>Bacteria</td>
-                        <td>Bacillus circulans WL-12</td>
-                        <td>chitinase A1 (ChiA;ChiA1)</td>
-                </tr>
-
-
-        </table>
+#         </table>
 
 
 
-if hits does not appear:
-report: not in cazy
+# if hits does not appear:
+# report: not in cazy
