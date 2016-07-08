666fd0bba5ea0c9a8ef952689d747ac617e5f6f1
Steven Lewis <smlewi@gmail.com>
2016-07-07 21:46:30 -0400

    data scraping working

--- cis_in_cazy/process_html.py
+++ cis_in_cazy/process_html.py
@@ -6,45 +6,25 @@ results = {}
 
 for each_html in os.listdir("raw_html"):
     f = open("raw_html/"+each_html, 'r').read()
+    #flines = f.splitlines()
     if "hits" in f:
         hits = False #flag for if we hit the right part of the file yet
-        for line in f:
+        #print each_html
+        results[each_html] = []
+        for line in f.splitlines():
+            #print line
             if "hits" in line:
                 hits = True
+                #print "hitsflag"
                 continue
             if hits:
                 if "<td>" in line:
+                    #print line
+                    results[each_html].append(line)
                     #results.append the next bit
                 #process
             if "</table>" in line:
+                print "done processing" + each_html
                 break #done processing the table
 
-#pseudocode for the rest of the project (do this in python)
-# grep hits 1ubq.html
-# if "hits" appears:
-
-# parse these bits out to csv
-
-# <table class="search">
-#                 <tr>
-#                         <th class="thsearch">Family</th>
-#                         <th class="thsearch">Kingdom</th>
-#                         <th class="thsearch">Organism</th>
-#                         <th class="thsearch">Protein Name</th>
-#                 </tr>
-
-#                 <tr>
-#                         <td><a href="http://www.cazy.org/GH18.html">
-#                                         GH18</a></td>
-#                         <td>Bacteria</td>
-#                         <td>Bacillus circulans WL-12</td>
-#                         <td>chitinase A1 (ChiA;ChiA1)</td>
-#                 </tr>
-
-
-#         </table>
-
-
-
-# if hits does not appear:
-# report: not in cazy
+print results
