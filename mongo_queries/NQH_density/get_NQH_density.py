0178114a0392454bfa92a54e554652e701ca4921
bhintze <bradley@hintze.org>
2016-11-29 17:28:46 -0500

    adding sort method to get_NQH_density.

--- mongo_queries/NQH_density/get_NQH_density.py
+++ mongo_queries/NQH_density/get_NQH_density.py
@@ -9,9 +9,9 @@ class NQH_density(object) :
     self.pdb_id = pdb_id
 
   def set_density_values(self,db) :
-    heads = ['pdb_id','chain_id','resseq','icode','altloc','resname']
-    heads+= ['worst_rscc','worst_2fo-fc','N','C_O','delta']
-    self.csv_lines = [heads]
+    #heads = ['pdb_id','chain_id','resseq','icode','altloc','resname']
+    #heads+= ['worst_rscc','worst_2fo-fc','N','C','O','delta']
+    self.csv_lines = []
     q = {"pdb_id":self.pdb_id,"resname":{"$in":["GLN","ASN","HIS"]}}
     cursor = db.residues_colkeys.find(q)
     for d in cursor :
@@ -19,24 +19,46 @@ class NQH_density(object) :
       ll+= [d["altloc"],d["resname"],"%.3f" % d["worst_all"]["rscc"]["value"]]
       ll+= ["%.3f" % d["worst_all"]["twoFo_DFc_value"]["value"]]
       if d["resname"] == "HIS" :
-        Na = d["atoms"]["ND1"]["twoFo_DFc_value"]
-        COa = d["atoms"]["CD2"]["twoFo_DFc_value"]
+        if not "ND1" in d["atoms"].keys() and not "CD2" in d["atoms"].keys() :
+          continue
+        Na = d["atoms"]["ND1"]["twoFo_DFc_value"] or ''
+        Ca = d["atoms"]["CD2"]["twoFo_DFc_value"]
+        Oa = ''
       elif d["resname"] == "GLN" :
+        if not "NE2" in d["atoms"].keys() and not "OE1" in d["atoms"].keys() :
+          continue
         Na = d["atoms"]["NE2"]["twoFo_DFc_value"]
-        COa = d["atoms"]["OE1"]["twoFo_DFc_value"]
+        Ca = ''
+        Oa = d["atoms"]["OE1"]["twoFo_DFc_value"]
       else :
+        if not "ND2" in d["atoms"].keys() and not "OD1" in d["atoms"].keys() :
+          continue
         Na = d["atoms"]["ND2"]["twoFo_DFc_value"]
-        COa = d["atoms"]["OD1"]["twoFo_DFc_value"]
-      ll+= ["%.3f" % Na,"%.3f" % COa,"%.3f" % (Na-COa)]
+        Ca = ''
+        Oa = d["atoms"]["OD1"]["twoFo_DFc_value"]
+      if d["resname"] == "HIS" :
+        ll+= ["%.3f" % Na,"%.3f" % Ca,Oa]
+        ll += ["%.3f" % (Na-Ca)]
+      else :
+        ll+= ["%.3f" % Na,Ca,"%.3f" % Oa]
+        ll += ["%.3f" % (Oa-Na)]
       #print ",".join(ll)
       self.csv_lines.append(ll)
 
   def write_csv(self,out=sys.stdout) :
+    heads = ['pdb_id','chain_id','resseq','icode','altloc','resname']
+    heads+= ['worst_rscc','worst_2fo-fc','N','C','O','delta']
+    exp = "# delta is the 2fo-fc values for the O - N for GLN and ASN and "
+    exp+= "N - C for HIS."
+    print >> out, exp
+    print >> out, ",".join(heads)
+    self.csv_lines.sort(key=lambda x: int(x[2]))
     for ll in self.csv_lines :
+      assert len(heads) == len(ll)
       print >> out, ",".join(ll)
 
 def run(args) :
-  desc = "Returns a csv of all map values ate atom positions of HQNs in the "
+  desc = "Returns a csv of all map values at atom positions of HQNs in the "
   desc+= "given PDB."
   parser = argparse.ArgumentParser(description=desc)
   parser.add_argument('pdb_id', help='A pdb code')
@@ -47,7 +69,7 @@ def run(args) :
   nqhD = NQH_density(args.pdb_id)
   nqhD.set_density_values(mongocon.db)
   nqhD.write_csv()
-# fastpig.r
+
 if __name__ == '__main__' :
   run(sys.argv[1:])
 
